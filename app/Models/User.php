264bcd420766f579b7477090bc4b639b7bcca5b8
Univers1994 <tononregis67@gmail.com>
2025-02-13 09:22:42 +0100

    Modif user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,11 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    protected $fillable = [
+        'name', 
+        'email', 
+        'password',
+    ];
 
     public function chirps(): HasMany
     {
