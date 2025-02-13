9cc9500cbdbbae26c0a878a56853e86c7b474efa
Univers1994 <tononregis67@gmail.com>
2025-02-13 08:19:36 +0100

    Modification

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,2 @@
+<h1>Bonjour !</h1>
+<p>Ceci est un test d'e-mail avec Mailpit et Laravel.</p>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Mail\TestEmail;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('chirps', ChirpController::class)
+    // ->only(['index', 'store'])
+    // ->only(['index', 'store', 'edit', 'update'])
+    ->only(['index', 'store', 'edit', 'update', 'destroy'])
+    ->middleware(['auth', 'verified']);
+
+    Route::get('/send-test-email', function () {
+        Mail::to('user@example.com')->send(new TestEmail());
+        return "E-mail envoyé ! Vérifie Mailpit.";
+    });
+
+
+require __DIR__.'/auth.php';
